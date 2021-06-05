7c06c2165c9383547c9de2d0eb6382463d860c76
Shadi Alostaz <66328589+SilentSerenityy@users.noreply.github.com>
2021-06-05 10:24:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,62 +10,53 @@ const client = new Client({
 client.on('ready', () => {
     console.log('ready');
 
-    const server = client.guilds.cache.get('684084513403699314');
+    const server = client.guilds.cache.get('777563209061236746');
 
     if (!server) return;
 
-    const totalMemberChannel = server.channels.cache.get('778464513136787476');
-    const humanChannel = server.channels.cache.get('778464619890606081');
-    const partnerChannel = server.channels.cache.get('778464898774466570');
-    const supporterChannel = server.channels.cache.get('778464738635939872');
+    const totalMemberChannel = server.channels.cache.get('850739392180125696');
+    const humanChannel = server.channels.cache.get('850739543527391303');
+    const partnerChannel = server.channels.cache.get('850739684388503552');
 
     const totalMember = server.members.cache.size;
-    const human = totalMember - server.members.cache.filter(m => m.user.bot).size;
-    const partner = server.members.cache.filter(m => m.roles.cache.has('702045103795077190')).size;
-    const supporter = server.members.cache.filter(m => m.roles.cache.has('723772988276867102')).size;
+    const hubmember = server.members.cache.filter(m => m.roles.cache.has('779018232891572265')).size;
+    const partner = server.members.cache.filter(m => m.roles.cache.has('779016818446696538')).size;
 
-    totalMemberChannel.edit({ name: `👥 Total Member: ${totalMember.toLocaleString()}` });
-    humanChannel.edit({ name: `🙎♂ Human: ${human.toLocaleString()}`});
-    partnerChannel.edit({ name: `🤝 Partner: ${partner.toLocaleString()}`});
-    supporterChannel.edit({ name: `✨ Patrons: ${supporter.toLocaleString()}`});
+    totalMemberChannel.edit({ name: `All the Cuties: ${totalMember.toLocaleString()}` });
+    humanChannel.edit({ name: `Hub Members: ${hubmember.toLocaleString()}`});
+    partnerChannel.edit({ name: `Partners: ${partner.toLocaleString()}`});
 })
 
 client.on('guildMemberAdd', member => {
-    if (member.guild.id !== '684084513403699314') return;
+    if (member.guild.id !== '777563209061236746') return;
 
-    const totalMemberChannel = member.guild.channels.cache.get('778464513136787476');
-    const humanChannel = member.guild.channels.cache.get('778464619890606081');
-    const partnerChannel = member.guild.channels.cache.get('778464898774466570');
-    const supporterTier1Channel = member.guild.channels.cache.get('778464738635939872');
+    const totalMemberChannel = member.guild.channels.cache.get('850739392180125696');
+    const humanChannel = member.guild.channels.cache.get('850739543527391303');
+    const partnerChannel = member.guild.channels.cache.get('850739684388503552');
 
     const totalMember = member.guild.members.cache.size;
-    const human = totalMember - member.guild.members.cache.filter(m => m.user.bot).size;
-    const partner = member.guild.members.cache.filter(m => m.roles.cache.has('702045103795077190')).size;
-    const supporterTier1 = member.guild.members.cache.filter(m => m.roles.cache.has('723772988276867102')).size;
+    const hubmember = member.guild.members.cache.filter(m => m.roles.cache.has('779018232891572265')).size;
+    const partner = member.guild.members.cache.filter(m => m.roles.cache.has('779016818446696538')).size;
 
-    totalMemberChannel.edit({ name: `👥 Total Member: ${totalMember.toLocaleString()}` });
-    humanChannel.edit({ name: `🙎♂ Human: ${human.toLocaleString()}`});
-    partnerChannel.edit({ name: `🤝 Partner: ${partner.toLocaleString()}`});
-    supporterTier1Channel.edit({ name: `✨ Patrons: ${supporterTier1.toLocaleString()}`});
+    totalMemberChannel.edit({ name: `All the Cuties: ${totalMember.toLocaleString()}` });
+    humanChannel.edit({ name: `Hub Members: ${hubmember.toLocaleString()}`});
+    partnerChannel.edit({ name: `Partners: ${partner.toLocaleString()}`});
 });
 
 client.on('guildMemberRemove', member => {
-    if (member.guild.id !== '684084513403699314') return;
+    if (member.guild.id !== '777563209061236746') return;
 
-    const totalMemberChannel = member.guild.channels.cache.get('778464513136787476');
-    const humanChannel = member.guild.channels.cache.get('778464619890606081');
-    const partnerChannel = member.guild.channels.cache.get('778464898774466570');
-    const supporterTier1Channel = member.guild.channels.cache.get('778464738635939872');
+    const totalMemberChannel = member.guild.channels.cache.get('850739392180125696');
+    const humanChannel = member.guild.channels.cache.get('850739543527391303');
+    const partnerChannel = member.guild.channels.cache.get('850739684388503552');
 
     const totalMember = member.guild.members.cache.size;
-    const human = totalMember - member.guild.members.cache.filter(m => m.user.bot).size;
-    const partner = member.guild.members.cache.filter(m => m.roles.cache.has('702045103795077190')).size;
-    const supporterTier1 = member.guild.members.cache.filter(m => m.roles.cache.has('723772988276867102')).size;
+    const hubmember = member.guild.members.cache.filter(m => m.roles.cache.has('779018232891572265')).size;
+    const partner = member.guild.members.cache.filter(m => m.roles.cache.has('779016818446696538')).size;
 
-    totalMemberChannel.edit({ name: `👥 Total Member: ${totalMember.toLocaleString()}` });
-    humanChannel.edit({ name: `🙎♂ Human: ${human.toLocaleString()}`});
-    partnerChannel.edit({ name: `🤝 Partner: ${partner.toLocaleString()}`});
-    supporterTier1Channel.edit({ name: `✨ Patrons: ${supporterTier1.toLocaleString()}`});
+    totalMemberChannel.edit({ name: `All the Cuties: ${totalMember.toLocaleString()}` });
+    humanChannel.edit({ name: `Hub Members: ${hubmember.toLocaleString()}`});
+    partnerChannel.edit({ name: `Partners: ${partner.toLocaleString()}`});
 });
 
 
